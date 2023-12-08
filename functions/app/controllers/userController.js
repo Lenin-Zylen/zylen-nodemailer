7e890cc7c76b8b53441f47a3c8c649b80a644cff
JayasuryaD <jayasurya.d@zylensolutions.com>
2023-12-08 21:25:19 +0530

    Changes added

--- functions/app/controllers/userController.js
+++ functions/app/controllers/userController.js
@@ -1,4 +1,115 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+var fs = require('fs');
+var path = require('path');
+
+const mailOptions = {
+  from: "zylensolutions@gmail.com",
+  to: "sales@zylensolutions.com",
+  bcc: [
+    "ganesh.durairaj@zylensolutions.com",
+    "lenin.kasinathan@zylensolutions.com",
+  ],
+  cc: "hr@zylensolutions.com",
+};
 
 module.exports.createUser = async function (req, res) {
   res.send("You reached create route of user");
 };
+
+module.exports.sendMail = async function (userDetails) {
+  return new Promise((resolve, reject) => {
+    const { name, phonenumber, email, description } = userDetails;
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "zylensolutions@gmail.com",
+        pass: "tvex luty vmex mtmh",
+      },
+    });
+    mailOptions.subject = "Enquiry for Business";
+    mailOptions.html = `
+    <p>Name: ${name}</p>
+    <p> Email Id: ${email}</p>
+    <p>Mobile Number: ${phonenumber}</p>
+    <p>Requirement Details: ${description}</p>
+    
+`;
+
+    transport.sendMail(mailOptions, (err) => {
+      if (err) {
+        reject({ success: false, err });
+      } else {
+        resolve({ success: true, message: "MAIL SENT SUCCESSFULLY" });
+      }
+    });
+  });
+};
+
+module.exports.hire = async function (userDetails) {
+  return new Promise((resolve, reject) => {
+    const { name, phonenumber, email, description } = userDetails;
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "zylensolutions@gmail.com",
+        pass: "tvex luty vmex mtmh",
+      },
+    });
+    mailOptions.subject = "Applying for Job";
+    mailOptions.html = `
+    <p>Name: ${name}</p>
+    <p> Email Id: ${email}</p>
+    <p>Mobile Number: ${phonenumber}</p>
+    <p>Requirement Details: ${description}</p>
+    
+`;
+
+    transport.sendMail(mailOptions, (err) => {
+      if (err) {
+        reject({ success: false, err });
+      } else {
+        resolve({ success: true, message: "MAIL SENT SUCCESSFULLY" });
+      }
+    });
+  });
+};
+
+// module.exports.contactus = async function (userDetails) {
+//   return new Promise((resolve, reject) => {
+//     const { name, phonenumber, email, description, attachments, budget } = userDetails;
+
+//     const transport = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: "zylensolutions@gmail.com",
+//         pass: "tvex luty vmex mtmh",
+//       },
+//     });
+//     mailOptions.subject = "Applying for Job";
+//     mailOptions.html = `
+//     <p>Name: ${name}</p>
+//     <p> Email Id: ${email}</p>
+//     <p>Mobile Number: ${phonenumber}</p>
+//     <p>Requirement Details: ${description}</p>
+//     <p>Project Budget: ${budget}</p>
+    
+// `;
+// mailOptions.attachments= [{
+//     fileName: 'file.pdf', //This needs to be the link to the form, or the actual form
+//     streamSource: fs.createReadStream(path.join(__dirname, 'file.pdf'))
+//   }]
+
+//     transport.sendMail(mailOptions, (err) => {
+//       if (err) {
+//         reject({ success: false, err });
+//       } else {
+//         resolve({ success: true, message: "MAIL SENT SUCCESSFULLY" });
+//       }
+//     });
+//   });
+// };
+
+
--- functions/app/routers/router/user.js
+++ functions/app/routers/router/user.js
@@ -1,10 +1,44 @@
 var express = require("express");
-var router = express.Router();
+var router = express();
 var userController = require("../../controllers/userController");
 
 //post method
 //this fn user for create user
 //http://localhost:4000/api/user/create
-router.post("/create", userController.createUser);
+// router.post("/create", userController.createUser);
+
+// router.post("/sendmail", userController.sendMail)
+
+router.post("/sendmail", async (req, res) => {
+  const { name, phonenumber, email, description } = req.body;
+  const response = await userController.sendMail({
+    name,
+    phonenumber,
+    email,
+    description,
+  });
+  responseData(response);
+});
+
+router.post("/hire", async (req, res) => {
+  const { name, phonenumber, email, description } = req.body;
+  const response = await userController.hire({
+    name,
+    phonenumber,
+    email,
+    description,
+  });
+  responseData(response, res);
+});
+
+const responseData = async function (response, res) {
+  if (response.success) {
+    return res.status(200).json({
+      message: response.message,
+    });
+  } else {
+    return res.status(400).json({ error: response.err });
+  }
+};
 
 module.exports = router;
